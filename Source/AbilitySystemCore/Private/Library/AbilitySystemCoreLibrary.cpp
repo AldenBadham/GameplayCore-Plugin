b52d3acee13522ab5688f3c24fb214b3e57d01c3
AldenBadham <alden.badham@gmail.com>
2025-10-25 01:55:31 +0200

    Fixed typo

--- Source/AbilitySystemCore/Private/Library/AbilitySystemCoreLibrary.cpp
+++ Source/AbilitySystemCore/Private/Library/AbilitySystemCoreLibrary.cpp
@@ -1,4 +1,4 @@
-﻿// // Licensed under the MIT License. See the LICENSE file in the project root for full license information.
+﻿// Licensed under the MIT License. See the LICENSE file in the project root for full license information.
 
 
 #include "Library/AbilitySystemCoreLibrary.h"
